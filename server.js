89430a5b893c7e9a74de7c5c03a835efb0a1dd33
Bona-Zeynalabid <bonazeynalabid75@gmail.com>
2026-04-12 21:21:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: (origin, callback) => callback(null, true),
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
